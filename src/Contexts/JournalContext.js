fb83e1dd092b127085e9dc2a07f531f9e27e25c7
Kai Kuller <kavakai@gmail.com>
2022-02-19 14:41:47 -0700

    Add journalContext file

---
+++ src/Contexts/JournalContext.js
@@ -0,0 +1,20 @@
+import React, { useState, createContext } from "react";
+
+export const JournalContext = createContext();
+
+const JournalProvider = (props) => {
+  
+  const [journal, setJournal] = useState([]);
+
+  const journalValue = { journal, setJournal }
+  
+
+  return (
+    <JournalContext.Provider value={journalValue}>
+      {props.children}
+    </JournalContext.Provider>
+  );
+};
+
+export default JournalProvider;
+
