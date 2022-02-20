dc94c0917b98470fed7e9adc2630effe23884e69
Gabby Recny <gjtrecny@gmail.com>
2022-02-19 18:08:26 -0600

    Implement slider using hooks

--- src/Pages/MoodPage/MoodPage.js
+++ src/Pages/MoodPage/MoodPage.js
@@ -1,13 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { MoodContext } from '../../Contexts/MoodContext';
+import { QuoteContext } from '../../Contexts/QuoteContext';
+// import Slider from '../../Components/Slider'
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 
 const MoodPage = () => {
 
+    const [ currentMood, setCurrentMood ] = useState(0);
+
     const { quote } = useContext(QuoteContext);
     const { mood, setMood } = useContext(MoodContext);
+
+    const handleChange = (value) => {
+        // event.preventDefault()
+        setCurrentMood(value)
+    
+    }
     
     return (
-        <section>
+        <section style={{width: '500px'}}>
             <h4>Mood Page</h4>
+            <p>Current Value: {currentMood}</p>
+            <Slider 
+                defaultValue='1'
+                min={1}
+                max={5}
+                value={currentMood}
+                onChange={value => handleChange(value)}     
+            />
+
         </section>
     );
 };
