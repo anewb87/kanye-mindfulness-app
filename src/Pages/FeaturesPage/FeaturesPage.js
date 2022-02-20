7c6874eaf04dd3dad303834b175a75e3e8e93f30
Gabby Recny <gjtrecny@gmail.com>
2022-02-19 21:53:30 -0600

    Add grid layout to feature page

--- src/Pages/FeaturesPage/FeaturesPage.js
+++ src/Pages/FeaturesPage/FeaturesPage.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './FeaturesPage.scss'
 
 const FeaturesPage = () => {
     return (
         <section className='feature-page'>
-            <Link to={'/mood'}>
-                <article>Mood</article>
-            </Link>
-            <Link to={'/journal'}>
-                <article>Journal</article>
-            </Link>
-            <Link to='/dashboard'>
-                <button>Dashboard</button>
-            </Link>
+            <section className='feature-tiles'>
+                <Link to={'/mood'}>
+                    <article className='single-feat-tile'>Mood</article>
+                </Link>
+                <Link to={'/journal'}>
+                    <article className='single-feat-tile'>Journal</article>
+                </Link>
+                <article className='single-feat-tile'>Meditation</article>
+                <article className='single-feat-tile'>Sleep Tracker</article>
+            </section>
+            <section className='dashboard-btn-section'>
+                <Link to='/dashboard'>
+                    <button>Dashboard</button>
+                </Link>
+            </section>
         </section>
     );
 };
