e5fb7e0fa25152206295609ba890e0f25a1fd5c9
Gabby Recny <gjtrecny@gmail.com>
2022-02-23 20:57:15 -0600

    Add animations to bears and refactor grid

--- src/Pages/FeaturesPage/FeaturesPage.js
+++ src/Pages/FeaturesPage/FeaturesPage.js
@@ -2,30 +2,30 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import moodBear from '../../Assets/Bears/mood-bear.jpg';
 import journalBear from '../../Assets/Bears/journal-bear.jpg';
-import './FeaturesPage.scss'
+import './FeaturesPage.scss';
 
 const FeaturesPage = () => {
     return (
         <section className='feature-page'>
             <section className='feature-tiles'>
                 <Link to={'/mood'}>
-                    <article className='single-feat-tile' data-testid='single-feature-tile'>
+                    <article className='single-feat-tile-1' data-testid='single-feature-tile'>
                         <img className='bear-image slide-right' src={moodBear} alt='mood bear feature icon to take to mood checkin page'/>
                         {/* <p className='feature-title'>Mood</p> */}
                     </article>
                 </Link>
                 <Link to={'/journal'}>
-                    <article className='single-feat-tile' id='featureTitle' data-testid='single-feature-tile'>
-                        <img className='bear-image' src={journalBear} alt='journal bear feature icon to take to mood checkin page' />
+                    <article className='single-feat-tile-2' id='featureTitle' data-testid='single-feature-tile'>
+                        <img className='bear-image slide-left' src={journalBear} alt='journal bear feature icon to take to mood checkin page' />
                         {/* <p className='feature-title'>Journal</p> */}
                     </article>
                 </Link>
-                <article className='single-feat-tile' data-testid='single-feature-tile'>
+                {/* <article className='single-feat-tile' data-testid='single-feature-tile'>
                      <p className='feature-title'>Meditation</p>
                 </article>
                 <article className='single-feat-tile' data-testid='single-feature-tile'>
                     <p className='feature-title'>Sleep Tracker</p>
-                </article>
+                </article> */}
             </section>
             <section className='dashboard-btn-section'>
                 <Link to='/dashboard' data-testid='dashboard-link'>
