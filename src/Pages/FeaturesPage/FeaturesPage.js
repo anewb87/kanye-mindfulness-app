610a2d6a40a7e37b2e09650915c768c79dacf8fb
Alexis Newby <alexis.newby@gmail.com>
2022-02-24 15:36:47 -0700

    Implement flip

--- src/Pages/FeaturesPage/FeaturesPage.js
+++ src/Pages/FeaturesPage/FeaturesPage.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import moodBear from '../../Assets/Bears/mood-bear.jpg';
-import moodHoverBear from '../../Assets/Bears/bw-mood.jpg';
+import backMoodBear from '../../Assets/Bears/bw-mood.jpg';
 import journalBear from '../../Assets/Bears/journal-bear.jpg';
+import journalBear2 from '../../Assets/Bears/journal-bear2.jpg'
+import backJournalBear from '../../Assets/Bears/bw-journal.jpg';
 import './FeaturesPage.scss';
 
 const FeaturesPage = () => {
@@ -13,10 +15,31 @@ const FeaturesPage = () => {
         <section className='feature-page'>
             <section className='feature-tiles'>
                 <Link to={'/mood'} style={linkStyle}>
-                        <img className='bear-image' src={moodBear} alt='mood bear feature icon to take to mood checkin page'/>
+                    <div className='flip-container' >
+                        <div className='flipper'>
+                            <div className='front'>
+                                <img className='bear-image' src={moodBear} alt='mood bear feature icon to take to mood checkin page'/>
+                            </div>
+                            <div className='back'>
+                                <img className='bear-image' src={backMoodBear} alt='black and white feature icon to take to mood checkin page' />
+                                <p>write it out</p>
+                            </div>
+                        </div>
+                    </div>
                 </Link>
                 <Link to={'/journal'} style={linkStyle}>
-                    <img className='bear-image' src={journalBear} alt='journal bear feature icon to take to mood checkin page' />
+                    <div className='flip-container'>
+                        <div className='flipper'>
+                            <div className='front'>
+                                <img className='bear-image' src={journalBear2} alt='journal bear feature icon to take to mood checkin page' />
+                            </div>
+                            <div className='back'>
+                                <img className='bear-image' src={backJournalBear} alt='black and white feature icon to take to mood checkin page' />
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+                <Link to={'/journal'} style={linkStyle}>
                 </Link>
             </section>
             <section className='dashboard-btn-section'>
