45fcb86bed2943d6ca7a891cf5caf1f6b7596197
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-02-25 13:32:04 -0500

    Update error page

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './ErrorPage.scss'
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <section>
-            <h2>"If everything I did failed — which it doesn't, it actually succeeds — just the fact that I'm willing to fail is an inspiration. People are so scared to lose that they don't even try."</h2>
-            <h3>Way to try. But you failed. Try again.</h3>
+        <section className='error-page'>
+            <h1>"If everything I did failed — which it doesn't, it actually succeeds — just the fact that I'm willing to fail is an inspiration. People are so scared to lose that they don't even try."</h1>
+            <p>Way to try. But you failed. Try again.</p>
+            <Link to="/dashboard"> Back to Home</Link>
         </section>
 
     )
