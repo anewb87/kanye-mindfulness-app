5c061bac8f4b7262adc41788a90a1e7de093e579
Alexis Newby <alexis.newby@gmail.com>
2022-02-24 19:00:34 -0700

    Refactored css for landing and features pages

--- src/Pages/FeaturesPage/FeaturesPage.js
+++ src/Pages/FeaturesPage/FeaturesPage.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 import moodBear from '../../Assets/Bears/mood-bear.jpg';
 import backMoodBear from '../../Assets/Bears/bw-mood.jpg';
 import journalBear from '../../Assets/Bears/journal-bear.jpg';
-import journalBear2 from '../../Assets/Bears/journal-bear2.jpg'
 import backJournalBear from '../../Assets/Bears/bw-journal.jpg';
+import Delay from '../../Components/Delay'
 import './FeaturesPage.scss';
 
 const FeaturesPage = () => {
@@ -14,7 +14,7 @@ const FeaturesPage = () => {
     return (
         <section className='feature-page'>
             <section className='feature-tiles'>
-                <Link to={'/mood'} style={linkStyle}>
+                <Link to={'/mood'} style={linkStyle} className='slide-up'>
                     <div className='flip-container' >
                         <div className='flipper'>
                             <div className='front'>
@@ -22,16 +22,15 @@ const FeaturesPage = () => {
                             </div>
                             <div className='back'>
                                 <img className='bear-image' src={backMoodBear} alt='black and white feature icon to take to mood checkin page' />
-                                <p>write it out</p>
                             </div>
                         </div>
                     </div>
                 </Link>
-                <Link to={'/journal'} style={linkStyle}>
+                <Link to={'/journal'} style={linkStyle} className='slide-down'>
                     <div className='flip-container'>
                         <div className='flipper'>
                             <div className='front'>
-                                <img className='bear-image' src={journalBear2} alt='journal bear feature icon to take to mood checkin page' />
+                                <img className='bear-image' src={journalBear} alt='journal bear feature icon to take to mood checkin page' />
                             </div>
                             <div className='back'>
                                 <img className='bear-image' src={backJournalBear} alt='black and white feature icon to take to mood checkin page' />
@@ -43,9 +42,11 @@ const FeaturesPage = () => {
                 </Link>
             </section>
             <section className='dashboard-btn-section'>
-                <Link to='/dashboard' data-testid='dashboard-link'>
-                  Dashboard
-                </Link>
+                <Delay>
+                    <Link to='/dashboard' data-testid='dashboard-link' className='dashboard-link fade-in-fwd'>
+                    Dashboard
+                    </Link>
+                </Delay>
             </section>
         </section>
     );
