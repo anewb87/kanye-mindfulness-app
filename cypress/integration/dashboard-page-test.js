02dced05b7340c56c4b2e22c98bd4da24677dc99
Kai Kuller <kavakai@gmail.com>
2022-02-27 10:05:10 -0700

    Refactor dashboard tests

--- cypress/integration/dashboard-page-test.js
+++ cypress/integration/dashboard-page-test.js
@@ -4,11 +4,18 @@ describe('Dashboard page', () => {
         cy.visit('http://localhost:3000/dashboard')
     });
 
-    it('Should welcome a user, display the quote of the day, and display buttons to navigate to other pages', () => {
-        cy.get('h2')
-            .should('have.text', 'Welcome, User! 💎 ')
+    it('Should welcome a user, display the quote of the day, and have navigation buttons to navigate to other pages', () => {
+        cy.get('h1')
+            .should('have.text', 'Welcome, Kaja!')
             .get('[data-testid=dashboard-quote]')
-            .should('have.text', 'You may be talented, but you’re not Kanye West.')
+            .should('have.text', '"You may be talented, but you’re not Kanye West."')
+            .get('[class=burger-button]')
+            .click()
+            .get('[class=menu]')
+            .should('be.visible')
+            .get('[class=dropdown-items]')
+            .contains('Vibe Check')
+            .click()
     });
 
 })
